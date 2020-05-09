1dbbe0ea2313e630f34a0a7b53bcc77c8d2dbc6f
Amelia Salgado <6043258+asalria@users.noreply.github.com>
2020-05-09 13:29:48 +0400

    Updates

---
+++ precache-manifest.f014a183514d6f3548f8a224ac9f5252.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944ce0ac195d208788ffec0a0ebcff16",
+    "url": "https://asalria.github.io/collections-web/index.html"
+  },
+  {
+    "revision": "867c69d929d49aedba8e",
+    "url": "https://asalria.github.io/collections-web/static/css/2.b6008db0.chunk.css"
+  },
+  {
+    "revision": "867c69d929d49aedba8e",
+    "url": "https://asalria.github.io/collections-web/static/js/2.f1d04543.chunk.js"
+  },
+  {
+    "revision": "c6c04685a5c32afe0e866b9e0e3fc4cc",
+    "url": "https://asalria.github.io/collections-web/static/js/2.f1d04543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150900108da7b2b0f41b",
+    "url": "https://asalria.github.io/collections-web/static/js/main.64b06377.chunk.js"
+  },
+  {
+    "revision": "b13411f2e1c86f2c782c",
+    "url": "https://asalria.github.io/collections-web/static/js/runtime-main.2a5d162a.js"
+  },
+  {
+    "revision": "b23d5960ea1aa29a34d567b62f9d1617",
+    "url": "https://asalria.github.io/collections-web/static/media/avatar.b23d5960.jpg"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "https://asalria.github.io/collections-web/static/media/mountains.586806cd.jpg"
+  }
+]);
