de01d13547da8e026401fd51f036ce777be4b683
Amelia Salgado <6043258+asalria@users.noreply.github.com>
2020-05-09 17:19:57 +0400

    fix: Signout

--- src/auth/index.js
+++ src/auth/index.js
@@ -49,7 +49,6 @@ export const signout = next => {
         method: 'GET'
     })
         .then(response => {
-            console.log('signout', response);
             return response.json();
         })
         .catch(err => console.log(err));
